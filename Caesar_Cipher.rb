6fd436b0d2abf6cd3c27bb79bf6c62141108b937
Charles Clemens <cclemensjr@gmail.com>
2016-12-20 08:06:10 -0800

    Added encryption feature

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,9 +1,27 @@
-def shift_alphabet
-  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-              'o', 'p', 'q', 'u', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def encrypt_message
+  @encrypted_message = @message.clone
 
-  shifted_alphabet = alphabet.clone
-  (0..@shift_factor - 1).each { shifted_alphabet.push(shifted_alphabet.shift) }
+  @message.each_index do |i|
+    @alphabet.each_index do |j|
+
+      if @message[i] == @alphabet[j]
+        @encrypted_message[i] = @shifted_alphabet[j]
+      end
+    end
+  end
+
+  puts "Encrypted Message: #{@encrypted_message.join(" ")}"
+end
+
+#******************
+#******************
+
+def shift_alphabet
+  @alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+  @shifted_alphabet = @alphabet.clone
+  (0..@shift_factor-1 ).each { @shifted_alphabet.push(@shifted_alphabet.shift) }
 end
 
 #******************
@@ -12,7 +30,7 @@ end
 def user_input
   puts "\n\nWelcome to the Encryptonomicron."
   print "\n\nMessage: "
-  @message = $stdin.gets.chomp.strip.split("")
+  @message = $stdin.gets.chomp.downcase!.strip.split("")
 
   print "\n\nShift factor: "
   @shift_factor = $stdin.gets.chomp.to_i
@@ -39,6 +57,8 @@ def use_the_cipher
   user_input
 
   shift_alphabet
+
+  encrypt_message
 end
 
 use_the_cipher
